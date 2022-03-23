8981732dbed38fb2c2557b7711470d7de29d5fb3
Marcelo Santos <rocha.mer21@gmail.com>
2022-03-23 14:25:53 -0300

    introdução ao Scaffold

--- app/src/main/java/xyz/spacermarcelo/movieapp/MainActivity.kt
+++ app/src/main/java/xyz/spacermarcelo/movieapp/MainActivity.kt
@@ -3,41 +3,51 @@ package xyz.spacermarcelo.movieapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import xyz.spacermarcelo.movieapp.ui.theme.MovieAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MovieAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+            MyApp {
+                MainContent()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MyApp(content: @Composable () -> Unit) {
+    MovieAppTheme {
+        Scaffold(topBar = {
+            TopAppBar(backgroundColor = Color.Yellow, elevation = 5.dp) {
+                Text(text = "Filmes")
+            }
+        }) {
+            content()
+        }
+    }
+}
+
+
+@Composable
+fun MainContent() {
+    Surface(color = MaterialTheme.colors.background) {
+        Text(text = "Hello ")
+    }
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    MovieAppTheme {
-        Greeting("Android")
+    MyApp {
+        MainContent()
     }
 }
+
