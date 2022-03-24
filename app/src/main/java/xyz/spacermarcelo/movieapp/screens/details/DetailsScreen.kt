ac7d811adee4f42d4f9a67dd1627d898377e0a44
Marcelo Santos <rocha.mer21@gmail.com>
2022-03-24 15:39:50 -0300

    criando um toolbar com back button

--- app/src/main/java/xyz/spacermarcelo/movieapp/screens/details/DetailsScreen.kt
+++ app/src/main/java/xyz/spacermarcelo/movieapp/screens/details/DetailsScreen.kt
@@ -1,29 +1,45 @@
 package xyz.spacermarcelo.movieapp.screens.details
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 
 @Composable
 fun DetailsScreen(navController: NavController, movie: String?) {
-    Surface(
-        modifier = Modifier
-            .fillMaxHeight()
-            .fillMaxWidth()
-    ) {
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
+    Scaffold(topBar = {
+        TopAppBar(backgroundColor = Color.Gray, elevation = 5.dp) {
+            Icon(
+                imageVector = Icons.Default.ArrowBack,
+                contentDescription = "Arrow back",
+                modifier = Modifier.clickable {
+                    navController.popBackStack()
+                })
+        }
+    }) {
+        Surface(
+            modifier = Modifier
+                .fillMaxHeight()
+                .fillMaxWidth()
         ) {
-            Text(text = "$movie", style = MaterialTheme.typography.h5)
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(text = "$movie", style = MaterialTheme.typography.h5)
+
+               
+            }
         }
     }
 
